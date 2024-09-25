a4a28c243aadb8b9cd813fb480a4f6f4cd48ddb0
Andy <zhumeilun0103@gmail.com>
2024-09-24 21:32:31 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework4_Part1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double Weight = Convert.ToDouble(weight.Text);
+            double Height = Convert.ToDouble(height.Text);
+
+            double BMI = Weight * 720 / (Height*Height);
+            BMI = Math.Round(BMI, 2);
+
+            label.Content = "Your BMI: "+BMI;
+
+            if (BMI > 26)
+            {
+                canvas.Background = Brushes.Yellow;
+            }
+            else if (BMI > 15)
+            {
+                canvas.Background = Brushes.Green;
+            }
+            else {
+                canvas.Background = Brushes.Blue;
+            }
+        }
+    }
+}
